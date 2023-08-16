4d1994c94043459a72aec115c6d4fda226365a8f
Nnafelix <obajifelix469@gmail.com>
2023-08-16 16:40:18 -0700

    5-sign_6

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -5,7 +5,9 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
+
 {
     int r;
 
@@ -27,7 +29,7 @@ int main(void)
     r = print_sign(-1);
     _putchar(',');
     _putchar(' ');
-    _putchar(r + '0');
+    _putchar(r + '0'
     _putchar('\n');
     return (0);
 }
