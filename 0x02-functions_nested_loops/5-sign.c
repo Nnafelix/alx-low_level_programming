f1206f730049ed429d485a08167628c11e4f6676
Nnafelix <obajifelix469@gmail.com>
2023-08-16 15:45:51 -0700

    correcteion on 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,28 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the number to check
- * Return: 1 for positive number, -1 for negetive number, zero for anything else
+ * @n: the int to check
+ * Return: 1 for positive number, -1 for negetive number abd zero for anything else
  */
+
 int print_sign(int n)
+
 {
-	
-        if (n>0)
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	} 
+	else if (n <0)
         {
-                _putchar(43);
-                return (1);
-        }
-        else if (n<0)
-        {
-                _putchar(45);
-                return (-1);
-        }
-        else
-        {
-                _putchar(48);
-                return (0);
+		_putchar(45);
+		return (-1);
+	} 
+	else 
+	{
+		_putchar(48);
+		return (0);
 	}
 }
+
+
